f45937991c07d78b6202118aaca82133ee329809
Russ Barth <rkbarth@gmail.com>
2026-01-30 21:25:04 +0000

    adding another test example

--- src/main/java/com/example/demo/config/DemoDataLoader.java
+++ src/main/java/com/example/demo/config/DemoDataLoader.java
@@ -15,7 +15,8 @@ import java.util.UUID;
 public class DemoDataLoader implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(DemoDataLoader.class);
-    public static final UUID DEMO_ID = UUID.fromString("11111111-1111-1111-1111-111111111111");
+    public static final UUID DEMO_ID1 = UUID.fromString("11111111-1111-1111-1111-111111111111");
+    public static final UUID DEMO_ID2 = UUID.fromString("22222222-2222-2222-2222-222222222222");
 
     private final CertificateService service;
 
@@ -25,8 +26,11 @@ public class DemoDataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        Certificate demo = new Certificate(DEMO_ID, "Demo Owner", "DEMO-SN-0001", Instant.now());
+        Certificate demo = new Certificate(DEMO_ID1, "Demo Owner1", "DEMO-SN-0001", Instant.now());
         service.create(demo);
-        log.info("Demo certificate created with id={}", DEMO_ID);
+        log.info("Demo certificate created with id={}", DEMO_ID1);
+        demo = new Certificate(DEMO_ID2, "Demo Owner2", "DEMO-SN-0002", Instant.now());
+        service.create(demo);
+        log.info("Demo certificate created with id={}", DEMO_ID2);
     }
 }
